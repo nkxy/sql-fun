ff2ac18d1fc288e37dbb7e1c88a0e19ae2a6ff37
Nick Yang <nick.yang3721@gmail.com>
2018-12-16 12:18:46 -0500

    more monkey changes

--- database.py
+++ database.py
@@ -11,9 +11,18 @@ def create_table():
 def add_casey():
     c.execute("INSERT INTO people VALUES('Casey Niu', 19)")
     conn.commit()
+
+
+def add_nick():
+    c.execute("INSERT INTO people VALUES('Nick Yang', 19)")
+    conn.commit()
+
+
+def close():
     c.close()
     conn.close()
 
 
 create_table()
-add_casey()
+add_nick()
+close()
