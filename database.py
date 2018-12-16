93b5bff9ae581058e51bb6b03905381498e8ce62
Nick Yang <nick.yang3721@gmail.com>
2018-12-16 15:24:38 -0500

    :sparkles: Added foreign keys to pairing

--- database.py
+++ database.py
@@ -24,7 +24,7 @@ participants = [Person('Casey', 19, 'candles'),
 def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS person(person_pk INTEGER PRIMARY KEY, name TEXT, age INTEGER, wish_list TEXT, date_created TEXT)')
     c.execute(
-        'CREATE TABLE IF NOT EXISTS pairing(to_person_fk INTEGER,from_person_fk INTEGER)')
+        'CREATE TABLE IF NOT EXISTS pairing(to_person_fk INTEGER, from_person_fk INTEGER, FOREIGN KEY (to_person_fk) REFERENCES person(person_pk), FOREIGN KEY (from_person_fk) REFERENCES person(person_pk))')
 
 
 def close():
@@ -46,6 +46,12 @@ def add_participants(participants):
         add_person(person)
 
 
+def add_pairing():
+    c.execute('INSERT INTO pairing (to_person_fk, from_person_fk) VALUES(1,2)')
+    conn.commit()
+
+
 create_table()
-add_participants(participants)
+add_pairing()
+# add_participants(participants)
 close()
