04d54da029eb2d1975eabc6e5ad0ef383ff5422f
Nick Yang <nick.yang3721@gmail.com>
2018-12-16 12:04:55 -0500

    Add basic database

--- database.py
+++ database.py
@@ -1,1 +1,19 @@
 import sqlite3
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS people(name TEXT, age REAL)')
+
+
+def add_casey():
+    c.execute("INSERT INTO people VALUES('Casey Niu', 19)")
+    conn.commit()
+    conn.close()
+    c.close()
+
+
+create_table()
+add_casey()
